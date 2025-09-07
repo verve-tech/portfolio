f11645a14a8a0067255f55fb211b778e77ec9832
Ghayas <ghayasleo99@gmail.com>
2025-09-07 21:04:16 +0500

    Fixed the header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -41,8 +41,8 @@ export default function Header() {
   const toggleMenu = () => setIsMenuOpen((prev) => !prev)
 
   return (
-    <header className="app-container flex items-center gap-20 lg:py-12.5 pt-7 pb-3 lg:relative sticky top-0 z-8 backdrop-blur-xs bg-background/90">
-      <Image src={logo} height={40} alt={process.env.NEXT_PUBLIC_APP_NAME || 'Company Logo'} />
+    <header className="header isolate app-container flex items-center gap-20 lg:py-12.5 pt-7 pb-3 lg:relative sticky top-0 z-8 bg-background/90">
+      <Image className='relative z-1' src={logo} height={40} alt={process.env.NEXT_PUBLIC_APP_NAME || 'Company Logo'} />
 
       <div
         className={cn(
@@ -54,7 +54,7 @@ export default function Header() {
 
       <div
         className={cn(
-          'bg-paper fixed top-0 right-0 z-10 flex h-[100vh] min-w-80 flex-col justify-between px-10 py-30 transition-all lg:contents',
+          'bg-paper fixed top-0 right-0 z-10 flex h-[100vh] min-w-80 flex-col justify-between px-10 pt-30 pb-10 transition-all lg:contents',
           isMenuOpen ? 'translate-x-0' : 'translate-x-[100%]'
         )}
       >
